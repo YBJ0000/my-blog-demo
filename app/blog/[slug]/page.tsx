bf0591ee74ebc772d8149b1ddb78125b6e13a526
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2025-03-16 12:34:13 +1100

    continue try fix timezone error

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import { client, urlFor } from '../../../lib/sanity'
 import Image from 'next/image'
 import Link from 'next/link'
-import { format } from 'date-fns'
-import { formatInTimeZone } from 'date-fns-tz'  // 修改这个导入
+import { formatInTimeZone } from 'date-fns-tz'
 import { PortableText } from '@portabletext/react'
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import { TypedObject } from '@portabletext/types'
@@ -50,7 +49,6 @@ async function BlogPost(props: PageProps) {
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 mb-4">
                 {post.title}
               </h1>
-              // 在组件中修改时间格式化部分
               <div className="text-gray-500">
                 By {post.author?.name} · {formatInTimeZone(
                   new Date(post.publishedAt),
